62e9b4826512a900291d508ec7c130873ce4cc2c
gonzales <gonzalesitokawa@gmail.com>
2023-04-08 15:41:09 +0900

    reafactor: gridArticles

--- src/src/pages/index.tsx
+++ src/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Header from "@/components/Haeder";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import CategoryBtn from "@/components/CategoryBtn";
 import GridArticles from "@/components/GridArticles";
